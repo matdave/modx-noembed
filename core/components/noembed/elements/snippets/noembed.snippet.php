42ea81f1680c2a94dca029a351d0c4c557c240b5
matdave <mat@matdave.com>
2018-05-30 18:48:37 -0500

    Build

---
+++ core/components/noembed/elements/snippets/noembed.snippet.php
@@ -0,0 +1,33 @@
+<?php
+$corePath = $modx->getOption('noembed.core_path', null, $modx->getOption('core_path', null, MODX_CORE_PATH) . 'components/noembed/');
+/** @var noembed $noembed */
+$noembed = $modx->getService(
+    'noembed',
+    'noembed',
+    $corePath . 'model/noembed/',
+    array(
+        'core_path' => $corePath
+    )
+);
+if (!($noembed instanceof noembed))
+    return;
+
+$response = $return = null;
+$link = $modx->getOption('link', $scriptProperties, null);
+$tpl = $modx->getOption('tpl', $scriptProperties, $options);
+
+if(!empty($input)){
+   $response = $noembed->getResponse($input);
+}
+
+if(!empty($link)){
+    $response = $noembed->getResponse($link);
+}
+
+if(!empty($tpl)){
+    $return =  $modx->getChunk($tpl, $response);
+}else{
+    $return = $response['html'];
+}
+
+return $return;
--- core/components/noembed/model/noembed/noembed.class.php
+++ core/components/noembed/model/noembed/noembed.class.php
@@ -5,3 +5,40 @@
  * Date: 5/29/2018
  * Time: 10:28
  */
+
+class noembed {
+
+    public $config = array();
+    function __construct(modX &$modx, array $config = array())
+    {
+        $this->modx =& $modx;
+        $corePath = $this->modx->getOption('noembed.core_path', $config, $this->modx->getOption('core_path') . 'components/noembed/');
+        $this->config = array_merge(array(
+            'basePath' => $this->modx->getOption('base_path'),
+            'corePath' => $corePath,
+            'modelPath' => $corePath . 'model/',
+            'snippetPatth' => $corePath . 'elements/snippets/',
+        ), $config);
+        $this->modx->addPackage('noembed', $this->config['modelPath']);
+    }
+
+    public function getResponse($link) {
+        if(empty($link)) return;
+
+        $url = 'https://noembed.com/embed?nowrap=on&url=' . urlencode($link);
+
+        $result = $this->fileGetContentsCurl($url);
+        
+        return $this->modx->fromJSON($result);
+    }
+
+    public function fileGetContentsCurl($url) {
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_URL, $url);
+        $data = curl_exec($ch);
+        curl_close($ch);
+        return $data;
+    }
+}
